97cfe6aa4425b2337fe9bcdf9dd7d78b0adf419f
markflm <38789101+markflm@users.noreply.github.com>
2020-02-16 12:47:47 -0500

    added routing for teams requests

---
+++ API/Routes/teams.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router(); //creates router from express package imported above
+const dbConn = require('./../../DataCreds.js')
+
+
+router.get('/:team/getusers', (req, res, next) =>{
+var teamName = req.params.team;
+ dbConn.query('SELECT DISTINCT UserName from userteams where Team = ?', teamName ,(err, result, fields) =>{
+    if (err) throw err;
+    console.log(result)
+     userList = result.map((item) => {
+        var user = item.UserName
+        return user
+    })
+    console.log(userList)
+    res.status(200).json({
+        message:'Users subscribed to ' + teamName,
+        users: userList
+    })
+  
+})
+})
+
+
+
+
+
+module.exports = router; //allow app.js to use redirect /team traffic here
--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 //'npm start' has been set to run project with nodemon server
-//TODO: fillout server.js; set up get and post methods; configure database credentials
 const express = require('express');
 const app = express(); //spins up express application
 const morgan = require('morgan'); //logging package
 const bodyPaser = require('body-parser');
 const dbConn = require('./DataCreds.js');
-
+const teamRoutes = require('./api/Routes/teams.js')
 
 dbConn.connect((err) => {
     if(err){
@@ -21,9 +20,10 @@ app.use(morgan('dev'));
 app.use(bodyPaser.urlencoded({extended:false})); //extended = 'rich text'. only want basic for now.
 app.use(bodyPaser.json()); //interprets incoming JSON data; put it right into an object without having to manually parse
 
+app.use('/teams', teamRoutes) //route requests to /teams to the teams.js file
 
 //response to "!teams" command, returns all teams a user is currently subscribed to
-app.get('/:user/getteams', (req, res, next) => {
+app.get('/user/:user/getteams', (req, res, next) => {
 var userName = req.params.user
 
 dbConn.query("SELECT * FROM UserTeams WHERE UserName = ?", userName, (err, result, fields) => {
@@ -41,7 +41,8 @@ res.status(200).json({
 )})
 
 //response to the "+[team]" command, adds incoming teams to the database for that user if they do not already exist.
-app.post('/:user/addteams',(req, res, next) => {
+//requires: one JSON object with a teamsToAdd key with value(s) in a string array
+app.post('/user/:user/addteams',(req, res, next) => {
     var userName = req.params.user
     var teamsToAdd = req.body.teamsToAdd;
 
@@ -53,32 +54,67 @@ dbConn.query("SELECT * FROM UserTeams WHERE UserName = ?", userName, (err, resul
         
     teamList = result.map(jsonifyTeams)
 
-    console.log(teamList)
+    //console.log(teamList) //teams user already has in database[]
     var teamsToInsert = teamsToAdd.filter(x => !teamList.includes(x))
-    console.log(teamsToInsert)
  
-    //logic that creates an INSERT statement that adds all these teams to the database
-    console.log(teamsToAdd)
-    res.status(201).json({
-        message:"teams added successfully for " + userName,
-        teams: teamsToInsert
-})
-//need a loop to add onto the insert query
-dbConn.query('INSERT INTO userteams (UserName, Team) VALUES (' +)
+    //if there are new teams to add, create an INSERT statement
+ if (teamsToInsert.length > 0) {   
+
+    var insertQueryText = 'INSERT INTO userteams (UserName, Team) VALUES '
+
+    //concat insert string together
+    for(i = 0; i < teamsToInsert.length; i++){
+
+        insertQueryText += "('" + userName + "','" + teamsToInsert[i] + "'),"
+    
+    }
+
+    //remove last comma for valid sql syntax
+    insertQueryText = insertQueryText.substr(0, insertQueryText.length - 1 )
+    //console.log(insertQueryText) //final insert query string being sent
+    dbConn.query(insertQueryText, (err, result, fields) => {
+        if (err) throw err;
+        res.status(201).json({
+            message: "Teams added successfully for " + userName,
+            teamsAdded: teamsToInsert
+     })
+ }) }
+ else{
+     res.status(202).json({
+         message:"User " + userName + " is already tracking all of these teams",
+         teamsPresent: teamsToAdd
+         })
+    }
 
     })
+     
 
 })
 
+
 //resposne to the "-[team]" command
-app.post('/:user/removeteams',(req, res, next) => {
+//requires: one JSON object with a teamsToRemove key with value(s) in a string array
+app.post('/user/:user/removeteams',(req, res, next) => {
     var userName = req.params.user;
     var teamsToRemove = req.body.teamsToRemove;
 
-        //add exceptions for when team isn't in users list
+
+var deleteString = ""
+for(i = 0; i < teamsToRemove.length;i++){
+    deleteString += "'" +teamsToRemove[i] + "',"
+}
+
+deleteString = deleteString.substr(0, deleteString.length - 1)
+
+
+    //todo: add response for when team isn't in users list
+    dbConn.query("DELETE FROM userteams WHERE team IN (" + deleteString + ") AND UserName = ?", userName, (err, result, fields) => {
+        if (err) throw err;
+        console.log(result)
+    })
     res.status(201).json({
         message:"Teams deleted successfully for " + userName,
-        teams: teamsToRemove
+        //teams: teamsToRemove //may need to do a 'get teams' on user to determine which teams were already in db for them, then return actual teams deleted
     })
 })
 
