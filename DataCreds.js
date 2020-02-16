fb4fdf8e4c91d9c9f238d391764ee51d1b29d797
markflm <38789101+markflm@users.noreply.github.com>
2020-02-16 13:48:12 -0500

    refactored to create pool instead of individual connection

--- DataCreds.js
+++ DataCreds.js
@@ -1,11 +1,21 @@
 var mysql = require('mysql');
 
-var con = mysql.createConnection({
+/* var con = mysql.createConnection({
+    pool: 10, //unsure how many of these we need, or if it helps at all
+    host:"us-cdbr-iron-east-04.cleardb.net",
+    user:"b8de33dafd8f78",
+    password: "8c3c1fd8",
+    database: "heroku_bba6983648ec3e9"
+}) */
+
+//may need to refactor api endpoints that require multiple queries if performance gets bad
+//move datacreds to env variable in heroku
+const con = mysql.createPool({
+    connectionLimit: 10,
     host:"us-cdbr-iron-east-04.cleardb.net",
     user:"b8de33dafd8f78",
     password: "8c3c1fd8",
     database: "heroku_bba6983648ec3e9"
 })
 
-
-module.exports = con
+module.exports = con;
