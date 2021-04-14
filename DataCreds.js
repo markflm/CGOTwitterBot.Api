d97630ce92aafa5fd14e8dc50e1b8df640d9db72
mark craven <mpcrav01@gmail.com>
2021-04-14 16:03:56 -0400

    moved db credentials to env variables

--- DataCreds.js
+++ DataCreds.js
@@ -1,21 +1,13 @@
 var mysql = require('mysql');
 
-/* var con = mysql.createConnection({
-    pool: 10, //unsure how many of these we need, or if it helps at all
-    host:"us-cdbr-iron-east-04.cleardb.net",
-    user:"b8de33dafd8f78",
-    password: "8c3c1fd8",
-    database: "heroku_bba6983648ec3e9"
-}) */
-
 //may need to refactor api endpoints that require multiple queries if performance gets bad
-//move datacreds to env variable in heroku
 const con = mysql.createPool({
-    connectionLimit: 25,
-    host:"us-cdbr-iron-east-04.cleardb.net",
-    user:"b8de33dafd8f78",
-    password: "8c3c1fd8",
-    database: "heroku_bba6983648ec3e9"
+    connectionLimit: process.env.mySqlConnLimit,
+    host: process.env.mySqlHost,
+    user: process.env.mySqlUser,
+    password: process.env.mySqlPassword,
+    database: process.env.mySqlDatabase
+
 })
 
 module.exports = con;
